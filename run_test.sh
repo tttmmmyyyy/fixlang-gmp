9946b209167dea98251411502e764ce8c4e3926b
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2023-05-19 08:01:01 +0900

    update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,6 @@
 # fix build test.fix --dynamic-link gmp --dynamic-link gmpfix && ./test
+cd libgmpfix
+sudo make install
+cd ..
 fix run test.fix --dynamic-link gmp --dynamic-link gmpfix
+echo "test end."
